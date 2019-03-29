1449aa3d1c63dfab8405f58cef72002209144d5c
Lingeshkumar1983 <34619678+Lingeshkumar1983@users.noreply.github.com>
2019-03-29 12:16:52 +0000

    Update summarize-enron.py

--- summarize-enron.py
+++ summarize-enron.py
@@ -41,25 +41,26 @@ df_raw_explode['month']=df_raw_explode['message_creation_dttime'].dt.month
 # df_recipient dataframe  - with receipient and its email count
 ###############################################################################################################
 
-df_sender=df_raw_explode.groupby(['sender','year'],as_index=True).agg({"time": "count"}).sort_values("time",ascending=False).reset_index().rename(columns={'time': 'sender_count'})
-df_recipient=df_raw_explode.groupby(['recipient','year'],as_index=True).agg({"time": "count"}).sort_values("time",ascending=False).reset_index().rename(columns={'time': 'recipient_count'})
+df_sender=df_raw_explode.groupby(['sender','year','month'],as_index=True).agg({"time": "count"}).sort_values("time",ascending=False).reset_index().rename(columns={'time': 'sender_count'}).fillna(0)
+df_recipient=df_raw_explode.groupby(['recipient','year','month'],as_index=True).agg({"time": "count"}).sort_values("time",ascending=False).reset_index().rename(columns={'time': 'recipient_count'}).fillna(0)
 
 ###############################################################################################################
 # Merge - outer join both sender and recipient dataframes and fill the null values to zero.
 ###############################################################################################################
 
-df_merged_sender_recipient=pd.merge(df_sender,df_recipient, how='outer',left_on=('year','sender'),right_on=('year','recipient'))
+df_merged_sender_recipient=pd.merge(df_sender,df_recipient, how='outer',left_on=('year','month','sender'),right_on=('year','month','recipient'))
 df_merged_sender_recipient['people'] = np.where(df_merged_sender_recipient["sender"].isnull() == True, df_merged_sender_recipient["recipient"], df_merged_sender_recipient["sender"] )
 df_merged_sender_recipient.drop(['recipient','sender'],axis=1,inplace=True)
-df_merged_sender_recipient=df_merged_sender_recipient[['year','people','sender_count','recipient_count']].sort_values('sender_count', ascending=False).fillna(0)
+df_merged_sender_recipient_year=df_merged_sender_recipient[['year','people','sender_count','recipient_count']].groupby(['people','year'],as_index=True).agg({"sender_count": "sum","recipient_count": "sum"}).sort_values('sender_count', ascending=False).reset_index().fillna(0)
+
 
 ###############################################################################################################
 # Add sender and recipient rank in descending counts. The highest count gets rank in that particular year
 ###############################################################################################################
 
-df_merged_sender_recipient['sender_count_rank'] = df_merged_sender_recipient.groupby(['year'])['sender_count'].rank("dense",ascending=False)
-df_merged_sender_recipient['recipient_count_rank'] = df_merged_sender_recipient.groupby(['year'])['recipient_count'].rank("dense",ascending=False)
-df_merge_out=df_merged_sender_recipient.groupby(['people'],as_index=True).agg({"sender_count": "sum","recipient_count": "sum"}).sort_values("sender_count",ascending=False).reset_index()
+df_merged_sender_recipient_year['sender_count_rank'] = df_merged_sender_recipient_year.groupby(['year'])['sender_count'].rank("dense",ascending=False)
+df_merged_sender_recipient_year['recipient_count_rank'] = df_merged_sender_recipient_year.groupby(['year'])['recipient_count'].rank("dense",ascending=False)
+df_merge_out=df_merged_sender_recipient_year.groupby(['people'],as_index=True).agg({"sender_count": "sum","recipient_count": "sum"}).sort_values("sender_count",ascending=False).reset_index()
 
 ###############################################################################################################
 # Solution1 output: Generate the First output file with People ( sender/recipient ) and its counts
@@ -69,7 +70,7 @@ df_merge_out.to_csv("out_sender_recepient_count.csv",index=False)
 ###############################################################################################################
 # Filter top 5 records with highest count for sender or recipient and plot the graph
 ###############################################################################################################
-Filtered_records=df_merged_sender_recipient[((df_merged_sender_recipient['sender_count_rank'] >= 1) & (df_merged_sender_recipient['sender_count_rank'] <= 5)) | ((df_merged_sender_recipient['recipient_count_rank'] >= 1) & (df_merged_sender_recipient['recipient_count_rank'] <= 5)) ]
+Filtered_records=df_merged_sender_recipient_year[((df_merged_sender_recipient_year['sender_count_rank'] >= 1) & (df_merged_sender_recipient_year['sender_count_rank'] <= 5)) | ((df_merged_sender_recipient_year['recipient_count_rank'] >= 1) & (df_merged_sender_recipient_year['recipient_count_rank'] <= 5)) ]
 
 ###############################################################################################################
 # Use the solution(1) output and plot to show top 10 senders bar diagram
@@ -86,3 +87,20 @@ for i in yearlist:
     plt.title('Top 10 sender & recipient count for year {}'.format(i))
     plt.xlabel("People")
     plt.savefig('Top_10_SendersandRecipients_for_{}.png'.format(i),bbox_inches='tight')
+
+###############################################################################################################
+# Solution3 output : Generate bar chart of sender/recipient at year/month level. Also generates difference
+# of sender and recpient at year/month level
+###############################################################################################################
+
+df_temp=df_merged_sender_recipient[['year','month','sender_count','recipient_count']].fillna(0).astype(int)
+df_plot3=df_temp.groupby(['year','month'],as_index=True).agg({"sender_count":"sum","recipient_count":"sum" }).sort_values('sender_count', ascending=False).reset_index().fillna(0)
+df_plot3['datetime']=pd.to_datetime(df_plot3['year'].astype(str)  + df_plot3['month'].astype(str), format='%Y%m')
+df_plot3.set_index('datetime', inplace=True)
+df_plot3.drop(['year','month'], axis=1, inplace=True)
+df_plot3.plot(kind='bar',figsize=(20,10), linewidth=5, fontsize=20)
+plt.savefig('Sender_recipient_yearmonth_count_breakdown.png',bbox_inches='tight')
+df_plot3['relative_recipient_diff_with_sender']= df_plot3['sender_count'] - df_plot3['recipient_count']
+df_plot3.drop(['sender_count','recipient_count'], axis=1, inplace=True)
+df_plot3.plot()
+plt.savefig('recipient_relative_difference_of_Sender_yearmonth.png',bbox_inches='tight')
